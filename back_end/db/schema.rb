4e96aea808be1604ee59a23ed9ab17f48ba39413
Romel Tan <romel.tan777@gmail.com>
2019-03-04 05:53:43 +0000

    added user model/table

--- back_end/db/schema.rb
+++ back_end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_054457) do
+ActiveRecord::Schema.define(version: 2019_03_04_055306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,14 @@ ActiveRecord::Schema.define(version: 2019_03_04_054457) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["car_id"], name: "index_prices_on_car_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "warranties", force: :cascade do |t|
