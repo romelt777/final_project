204a32f011f5d161325d309b57b13eb3b9bb9d64
Arthur Lam <lam.arthur0311@gmail.com>
2019-03-04 18:11:57 +0000

    created for setting up wireframe

---
+++ front_end/src/Components/Body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return(
+      <body>
+        <p>We are here to solve your vehicle financial issues with our analytics</p>
+        <button type="button" class="btn btn-info">Get Started</button>
+      </body>
+    );
+  }
+}
+
+export default Body;
