3fd74c6bafd03db204dffa381ae2094732a50825
Arthur Lam <lam.arthur0311@gmail.com>
2019-03-04 21:44:58 +0000

    modified for implementing image slider

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import Body from './Components/Body'
-import CarList from './Components/CarList';
+
+// for image slide show display effect
+import Bmw from './images/bmw.jpg';
+import Mazda from './images/mazda.jpg';
+import Honda from './images/honda.jpg';
+import Toyota from './images/toyota.jpg';
+import { Fade } from 'react-slideshow-image';
 
 class App extends Component {
   render() {
+    const fadeProperties = {
+      duration: 4000,
+      transitionDuration: 500,
+      infinite: true,
+      arrows: false,
+      autoplay: true
+    }
+
     return (
       <div className="App">
+        <Fade {...fadeProperties}>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={Bmw} alt="Logo" />
+            </div>
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={Mazda} alt="Logo" />
+            </div>
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={Honda} alt="Logo" />
+            </div>
+          </div>
+          <div className="each-fade">
+            <div className="image-container">
+              <img src={Toyota} alt="Logo" />
+            </div>
+          </div>
+        </Fade>
         <Body />
-        <CarList />
       </div>
     );
   }
--- front_end/src/Components/Body.js
+++ front_end/src/Components/Body.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 class Body extends Component {
+
   render() {
     return(
-      <body>
+      <div className="container">
         <p>We are here to solve your vehicle financial issues with our analytics</p>
-        <button type="button" class="btn btn-info">Get Started</button>
-      </body>
+        <button type="button" className="btn btn-info">Get Started</button>
+      </div>
     );
   }
 }
