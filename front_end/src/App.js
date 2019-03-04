3a9e5edbb4454155206d3d776f9341a587fa7029
Romel Tan <romel.tan777@gmail.com>
2019-03-04 17:13:56 +0000

    added router, works w/ no nested routes

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CarList from './Components/CarList'
 
-class App extends Component {
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Users() {
+  return (
+    <h2>Cars
+    <CarList /> </h2>
+  )
+}
+
+
+class AppRouter extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <CarList />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Route path="/about/" component={About} />
+          <Route path="/cars/" component={Users} />
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default AppRouter;
