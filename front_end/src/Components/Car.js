ccde22f016b0850acb734865139f44d91129b5ee
Romel Tan <romel.tan777@gmail.com>
2019-03-05 17:59:10 +0000

    cleaned code

--- front_end/src/Components/Car.js
+++ front_end/src/Components/Car.js
@@ -8,8 +8,7 @@ export default class CarList extends React.Component {
   }
 
   componentDidMount(){
-    console.log("hello", this.props.match)
-    console.log(this.props.match.params.id)
+
 
     axios.get(`http://localhost:3001/api/v1/cars/`)
       .then(res => {
@@ -30,23 +29,11 @@ export default class CarList extends React.Component {
         console.log("hello", newCar)
       }
     })
-    console.log(this.state.newCars)
 
     const newCars = this.state.newCars.reverse().map(car => (
       <h2 key={car.id}>{car.year} {car.make} {car.model}</h2>
     ))
 
-    console.log(newCars)
-
-    if(this.props.match.params){
-      console.log("exists")
-    } else{
-      console.log('nope')
-    }
-
-
-    console.log(this.state.newCars);
-    console.log("hello", this.props.match.params.id)
     return (
       <div>
         <ul>
