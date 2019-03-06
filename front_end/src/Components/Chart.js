9716cfc26aec320e9398f63e92a892d5fab4b2a1
Arthur Lam <lam.arthur0311@gmail.com>
2019-03-06 20:37:53 +0000

    successfully plot depi data passed down from App.js

--- front_end/src/Components/Chart.js
+++ front_end/src/Components/Chart.js
@@ -3,37 +3,50 @@
 import React from 'react';
 import {Line} from 'react-chartjs-2';
 
-const data = {
-  labels: ['1st year', '2nd year', '3rd year', '4th year', '5th year'],
-  datasets: [
-    {
-      label: 'dataset 1',
-      fill: false,
-      lineTension: 0,
-      backgroundColor: 'rgba(75,192,192,0)',
-      borderColor: 'rgba(75,192,192,1)',
-      data: [65, 59, 80, 81, 56]
-    },
-    {
-      label: 'dataset 2',
-      fill: false,
-      lineTension: 0,
-      backgroundColor: 'rgba(100,10,0,0)',
-      borderColor: 'rgba(100,10,0,1)',
-      data: [54, 99, 30, 40, 20]
-    }
-  ]
-};
+
+const amountOfPlot = (amount) => {
+
+}
+
+const chartData = (dataset) => {
+  return (
+    {labels: ['1st year', '2nd year', '3rd year', '4th year', '5th year'],
+    datasets: [
+      {
+        label: 'dataset 1',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: 'rgba(75,192,192,0)',
+        borderColor: 'rgba(75,192,192,1)',
+        data: dataset[0]
+      },
+      {
+        label: 'dataset 2',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: 'rgba(100,10,0,0)',
+        borderColor: 'rgba(100,10,0,1)',
+        data: dataset[2]
+      }
+    ]}
+  );
+}
 
 
 export default class Chart extends React.Component {
-  displayName: 'LineExample'
-
   render() {
+    // fetch prop depi passed down from App.js
+    // outputting arrays of data array
+    let depiData = [];
+    if(this.props.depi.length > 0) {
+      depiData = this.props.depi.map((data) =>
+        [data.first, data.second, data.third, data.fourth, data.fifth]);
+    }
+
     return (
       <div style={{width: 768, height: 768}}>
         <h2>Break even analysis</h2>
-        <Line data={data} />
+        <Line data={chartData(depiData)} redraw/>
       </div>
     );
   }
