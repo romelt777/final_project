870eb8284225193f88611c7e06c3ef7659947f02
Romel Tan <romel.tan777@gmail.com>
2019-03-06 23:45:48 +0000

    comparison component is making call to back end: 404

--- front_end/src/Components/Comparison.js
+++ front_end/src/Components/Comparison.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import axios from 'axios'
 
 export default class Comparison extends React.Component {
+  constructor(){
+    super()
 
+  }
+
+  componentDidMount(){
+    console.log(this.props.match.params.id)
+    axios.get(`http://localhost:3001/api/v1/comparisons/${this.props.match.params.id}`)
+      .then(res => {
+        const newCars = this.state.newCars
+        Array.prototype.push.apply(newCars, res.data.data)
+        this.setState({newCars})
+      });
+  }
 
   render(){
 
