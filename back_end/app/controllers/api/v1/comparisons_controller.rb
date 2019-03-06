32b3883f5aaa1226d78444a856988d622b96ab8c
Romel Tan <romel.tan777@gmail.com>
2019-03-06 20:26:48 +0000

    added function to save, but no comparisons table

--- back_end/app/controllers/api/v1/comparisons_controller.rb
+++ back_end/app/controllers/api/v1/comparisons_controller.rb
@@ -2,16 +2,26 @@ module Api
   module V1
     class ComparisonsController < ApplicationController
       def create
-        puts params
+        puts params[:_json]
+        # @comparison = Comparison.new(comparison_params)
+        # if @comparison.save
+        #   render json: {
+        #     status: 'SUCCESS',
+        #     message: 'Saved comparison to database',
+        #   },status: :created
+        # else
+        #   render json: {
+        #     status: 'FAILED',
+        #     message: 'Failed to save comparison car to database',
+        #   },status: :bad_request
+        # end
       end
-
-
 
 
       private
 
-      def comparisons_params
-        params[:_json][0].permit(:make, :model, :year, :condition)
+      def comparison_params
+        params[:_json][0].permit(:make, :model, :year, :condition, :newCars)
       end
 
     end
