1033d853b808fcbe44846837e3f1597313e389f1
Arthur Lam <lam.arthur0311@gmail.com>
2019-03-06 00:09:50 +0000

    included description on top

--- front_end/src/Components/Chart.js
+++ front_end/src/Components/Chart.js
@@ -1,8 +1,10 @@
+// chart component to show the break even analysis on different cars
+
 import React from 'react';
 import {Line} from 'react-chartjs-2';
 
 const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  labels: ['1st year', '2nd year', '3rd year', '4th year', '5th year'],
   datasets: [
     {
       label: 'dataset 1',
@@ -10,7 +12,7 @@ const data = {
       lineTension: 0,
       backgroundColor: 'rgba(75,192,192,0)',
       borderColor: 'rgba(75,192,192,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
+      data: [65, 59, 80, 81, 56]
     },
     {
       label: 'dataset 2',
@@ -18,7 +20,7 @@ const data = {
       lineTension: 0,
       backgroundColor: 'rgba(100,10,0,0)',
       borderColor: 'rgba(100,10,0,1)',
-      data: [54, 99, 30, 40, 20, 75, 40]
+      data: [54, 99, 30, 40, 20]
     }
   ]
 };
@@ -30,7 +32,7 @@ export default class Chart extends React.Component {
   render() {
     return (
       <div style={{width: 768, height: 768}}>
-        <h2>Line Example</h2>
+        <h2>Break even analysis</h2>
         <Line data={data} />
       </div>
     );
