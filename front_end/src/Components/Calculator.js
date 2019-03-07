42dddc0cc78e5cb1bc05d7b24e49bc9739b5576e
Arthur Lam <lam.arthur0311@gmail.com>
2019-03-07 21:13:40 +0000

    completed total payment calculator

--- front_end/src/Components/Calculator.js
+++ front_end/src/Components/Calculator.js
@@ -1,65 +1,66 @@
 import React, { Component } from 'react';
 
 export default class Calculator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      provincesTaxes: {
-          "AB": [["GST", 5]],
-          "BC": [["PST", 7], ["GST", 5]],
-          "YT": [["GST", 5]],
-          "ON": [["HST", 13]],
-          "MB": [["PST", 7], ["GST", 5]],
-          "NB": [["HST", 13]],
-          "NF": [["HST", 13]],
-          "SK": [["PST", 5], ["GST", 5]],
-          "QC": [["QST", 9.975], ["GST", 5]],
-          "PE": [["HST", 14]],
-          "NS": [["HST", 15]],
-          "NT": [["GST", 5]],
-          "NU": [["GST", 5]]
-      }
+
+
+  render() {
+
+    let vehiclePrice = 0;
+    let priceFAD = 0;
+    let subtotal = 0;
+    let hst = 0;
+    let priceTotal = 0;
+
+    if(this.props.price.length > 0) {
+      vehiclePrice = this.props.price[1].vehicle_price;
+      priceFAD = this.props.price[1].freight_delivery;
+      subtotal = vehiclePrice + priceFAD;
+      hst = vehiclePrice * 0.13;
+      priceTotal = hst + subtotal;
+
     }
 
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (evt) => {
-    this.setState({provincesTaxes: evt.target.value})
-    evt.preventDefault();
-
-  }
-  render() {
     return (
-    <div ClassName='priceCalculator'>
-      <h3>Payment Calculator</h3>
-      <div ClassName='provinceSelectorWrapper'>
-        <select id='provinceSelector' onChange={this.handleChange}>
-          <option value='AB'>AB</option>
-          <option value='BC'>BC</option>
-          <option value='MB'>MB</option>
-          <option value='NB'>NB</option>
-          <option value='NS'>NS</option>
-          <option value='ON'>ON</option>
-          <option value='PE'>PE</option>
-          <option value='QC'>QC</option>
-          <option value='NT'>NT</option>
-          <option value='NU'>NU</option>
-          <option value='SK'>SK</option>
-          <option value='YT'>YT</option>
-        </select>
+      <div ClassName='priceCalculator'>
+        <h3>Payment Calculator</h3>
+        <table ClassName='priceCalculator'>
+          <tbody>
+            <tr ClassName='price'>Vehicle Price: ${vehiclePrice.toFixed(2)}</tr>
+            <tr ClassName='priceTax'>Freight & Delivery: ${priceFAD.toFixed(2)}</tr>
+            <tr ClassName='priceTax'>Subtotal: ${subtotal.toFixed(2)}</tr>
+            <tr ClassName='priceTax'>HST (13%): ${hst.toFixed(2)}</tr>
+            <tr ClassName='priceTotal'>Total Price:* ${priceTotal.toFixed(2)}</tr>
+            <tr ClassName='calDisclaimer'>* This price excludes promotions and may vary depending on taxes and applicable fees.</tr>
+          </tbody>
+        </table>
+
+        <div ClassName='optTabs'>
+          <ul ClassName='contentItems'>
+            <li ClassName='leaseItem'>
+              <table ClassName='leaseCalculator'>
+                <tbody>
+                  <tr>Interest Rate (%):</tr>
+                  <tr>Term (Months):</tr>
+                  <tr>Down Payment ($):</tr>
+                  <tr>Monthly Payment:</tr>
+                </tbody>
+              </table>
+            </li>
+            <li ClassName='finItem'>
+              <table ClassName='finCalculator'>
+                <tbody>
+                  <tr>Interest Rate (%):</tr>
+                  <tr>Term (Months):</tr>
+                  <tr>Down Payment ($):</tr>
+                  <tr>Monthly Payment:</tr>
+                </tbody>
+              </table>
+            </li>
+          </ul>
+        <table>
+        </table>
+        </div>
       </div>
-      <table ClassName='priceCalculator'>
-        <tbody>
-          <tr ClassName='price'>Vehicle Price: </tr>
-          <tr ClassName='priceTax'>Freight & Delivery: </tr>
-          <tr ClassName='priceTax'>Subtotal: </tr>
-          <tr ClassName='priceTax'>HST(13%): </tr>
-          <tr ClassName='priceTotal'>Total Price:* </tr>
-          <tr ClassName='calDisclaimer'>* This price excludes promotions and may vary depending on taxes and applicable fees.</tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    );
   }
 }
