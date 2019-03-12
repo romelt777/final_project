1de0fdcc6decbe0b4a76f11f3147556815aba9de
Romel Tan <romel.tan777@gmail.com>
2019-03-12 05:01:01 +0000

    chart able to show repairs, but missing valuues

--- front_end/src/Components/Comparison.js
+++ front_end/src/Components/Comparison.js
@@ -44,6 +44,8 @@ export default class Comparison extends React.Component {
       this.setState({toggle: "maintenances"})
     } else if(event.target.value == 2){
       this.setState({toggle: "newDepreciations"})
+    } else if(event.target.value == 3){
+      this.setState({toggle: "repairs"})
     }
   }
 
@@ -68,10 +70,12 @@ export default class Comparison extends React.Component {
           <ToggleButtonGroup type="radio" name="options" defaultValue={1} >
             <ToggleButton value={1} onChange={this.toggle}>Depreciation </ToggleButton>
             <ToggleButton value={2} onChange={this.toggle}>Maintenance</ToggleButton>
+            <ToggleButton value={3} onChange={this.toggle}>Repairs</ToggleButton>
           </ToggleButtonGroup>
         </ButtonToolbar>
         { this.state.toggle === "maintenances" ? <Chart carName={carName} data={this.checkData(this.props.data.maintenances)}/>
           : this.state.toggle === "newDepreciations" ? <Chart carName={carName} data={this.checkData(this.props.data.newDepreciations)}/>
+          : this.state.toggle === "repairs" ? <Chart carName={carName} data={this.checkData(this.props.data.repairs)}/>
           : null
         }
 
