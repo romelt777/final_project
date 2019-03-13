978cb9345f229a2600145b92977f31fe4865c0b6
Arthur Lam <lam.arthur0311@gmail.com>
2019-03-13 04:20:51 +0000

    able to extract car comparsion data

--- front_end/src/Components/Datatable.js
+++ front_end/src/Components/Datatable.js
@@ -8,9 +8,6 @@ export default class Datatable extends React.Component {
 
   render() {
 
-    console.log(this.props.data)
-    // console.log(this.props.comparison)
-
     // store car id in comparison
     let comparison = []
     if(this.props.comparison.length > 0) {
@@ -19,11 +16,21 @@ export default class Datatable extends React.Component {
       })
     }
 
+    // store car img in img
+    let imgs = []
+    if(this.props.data.newPictures.length > 0) {
+      this.props.data.newPictures.reverse().map((pic) => {
+        if(comparison.includes(pic.id)) {
+          imgs.push(pic.url)
+        }
+      })
+    }
+
     // store car name in cars
     let cars = []
     if(this.props.data.cars.length > 0) {
       this.props.data.cars.reverse().map((car) => {
-        if(comparison.includes(car.id)){
+        if(comparison.includes(car.id)) {
           let carname = car.year + ' ' + car.make + ' ' + car.model
           cars.push(carname)
         }
@@ -31,35 +38,58 @@ export default class Datatable extends React.Component {
     }
 
     // store car price
-    // store car fuel economy
-    // store car warranty
+    let prices = []
+    if(this.props.data.newPrices.length > 0) {
+      this.props.data.newPrices.map((price) => {
+        if(comparison.includes(price.id)) {
+          let formatPrice = '$' + price.vehicle_price.toFixed(2)
+          prices.push(formatPrice)
+        }
+      })
+    }
 
-    // generateTh = (array) => {
-    //   return (
-    //     <th>
-    //     </th>
-    //   )
-    // }
+    // store car fuel economy
+    let fuels = []
+    if(this.props.data.newFuels.length > 0) {
+      this.props.data.newFuels.map((fuel) => {
+        if(comparison.includes(fuel.id)) {
+          fuels.push(fuel)
+        }
+      })
+    }
+
+    // store car warranty
+    let warranties = []
+    if(this.props.data.newWarranties.length > 0) {
+      this.props.data.newWarranties.map((warranty) => {
+        if(comparison.includes(warranty.id)) {
+          warranties.push(warranty)
+        }
+      })
+    }
 
     return (
       <Col sm={12}>
         <Table striped bordered hover>
            <thead>
             <tr>
-              <th>First car Name</th>
-              <th>Last car Name</th>
+              <th>1</th>
+              <th>2</th>
             </tr>
           </thead>
           <tbody>
             <tr>
+              <td>Price</td>
               <td>Mark</td>
               <td>Otto</td>
             </tr>
             <tr>
+              <td>Fuel Economy</td>
               <td>Jacob</td>
               <td>Thornton</td>
             </tr>
             <tr>
+              <td>Warranty</td>
               <td>3</td>
               <td>@twitter</td>
             </tr>
